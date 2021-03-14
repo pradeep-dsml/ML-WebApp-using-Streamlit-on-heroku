0accae0a372d6b1ddf8f8a3bf22b022c606c4c8b
PRADEEP YADAV <74373064+pradeep-dsml@users.noreply.github.com>
2021-03-14 23:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = MultiApp()
 
 st.markdown("""
 # Multi-Page App
-
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-
+This is muli-page app where you can select an app from multiple apps
 """)
 
 # Add all your application here
